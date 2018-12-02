9e09d9ae1351e2219d659e26bcf9e105cda981a7
xiezhiping <1529464401@qq.com>
2018-12-02 16:38:05 +0800

    comment

--- src/dp/LCSS2.java
+++ src/dp/LCSS2.java
@@ -6,7 +6,7 @@ public class LCSS2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		while(sc.hasNext()) {
-			String str1 = sc.next();
+			String str1 = sc.next(); // 获取以空格分割的字符串
 			String str2 = sc.next();
 			System.out.println(length(str1.toCharArray(), str2.toCharArray()));
 		}
@@ -17,23 +17,23 @@ public class LCSS2 {
 		int[][] dp = new int[m][n];
 		for (int i = 0; i < m;i++) {
 			for (int j = 0;j < n; j++) {
-				if ( i < 1) {
-					if (str1[0] == str2[j] || str1[0] == str2[0]) {
+				if ( i < 1) { // 第一行
+					if (str1[0] == str2[j] || str1[0] == str2[0]) { // 边界值容易错
 						dp[i][j] = 1;
 					} else {
 						dp[i][j] =0;
 					}
-				} else if (j < 1) {
+				} else if (j < 1) { // 第一列
 					if (str1[i] == str2[j] || str2[0] == str1[0] || dp[i-1][0] == 1) {
 						dp[i][j] = 1;
 					} else {
 						dp[i][j] =0;
 					}
-				} else {
+				} else { // m、n大于等于1的情况
 					if (str1[i] == str2[j]) {
-						dp[i][j] = dp[i-1][j-1] + 1;
+						dp[i][j] = dp[i-1][j-1] + 1; // 先加入的两个字符相等的情况
 					} else {
-						dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+						dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]); // 不等的情况取左边格子、上边格子两个中的较大值
 					}
 				}
 				
