281b03c23464ecea705d7d418d5a140e9dcc1011
xiezhiping <1529464401@qq.com>
2018-12-06 16:17:32 +0800

    bak

--- src/dp/MonoSum.java
+++ src/dp/MonoSum.java
@@ -3,7 +3,6 @@ package dp;
 public class MonoSum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int[] A = {1,3,5,2,4,6};
 		System.out.println(new MonoSum().calcMonoSum(A,6));
 	}
@@ -12,20 +11,10 @@ public class MonoSum {
         int sum = 0; // 相加的结果
         int[] dp = new int[n]; // 存储f(i)
         dp[0] = 0; // 第一个初始化为0
+        short s = 1;
+        long num = 99999999999999L;  // error
+        System.out.println(s);
         for (int i = 1; i < n; i++) {
-//            dp[i] = 0; // 初始化
-//            // 分两种情况
-//            if (A[i] >= A[i-1]) {
-//                dp[i] = dp[i-1] + A[i-1];
-//                sum += dp[i];
-//            } else {
-//                for (int j = 0; j < i; j++) {
-//                    if (A[i] >= A[j]) {
-//                        dp[i] += A[j];
-//                    }
-//                }
-//                sum += dp[i];
-//            }
         	 for (int j = 0; j < i; j++) {
                  if (A[i] >= A[j]) {
                      sum += A[j];
