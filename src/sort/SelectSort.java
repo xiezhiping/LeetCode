cf2002e06e8d6633cc5a11ed6773223ca61ce9a8
xiezhiping <1529464401@qq.com>
2018-12-21 09:38:02 +0800

    select sort

---
+++ src/sort/SelectSort.java
@@ -0,0 +1,39 @@
+package sort;
+/**
+ * 交换操作介于0~(n-1)之间；
+ * 比较操作为(n-1)*n/2;
+ * 赋值操作介于0~(n-1)*3；
+ * 平均复杂度为O(n^2).
+ * @author xzp
+ *
+ */
+public class SelectSort {
+
+	public static void main(String[] args) {
+		int[] a = {2, 5, 8, 9, 7, 6, 11, 10, -9, 3};
+		int length = a.length; // 放到for循环不恰当
+		System.out.println("before sort:");
+		for (int i= 0; i < length; i++) {
+			System.out.print(a[i] + " ");
+		}
+		selectSort(a, length);
+		System.out.println("\nafter sort:");
+		for (int i= 0; i < length; i++) {
+			System.out.print(a[i] + " ");
+		}
+	}
+	public static void selectSort(int[] a, int length) {
+		for (int i = 0; i < length; i++) {
+			for (int j = i +1; j < length; j++) {
+				if (a[i] > a[j]) {
+					swap(a, i, j);
+				}
+			}
+		}
+	}
+	public static void swap(int[] source, int start, int end) {
+		int temp = source[start];
+		source[start] = source[end];
+		source[end] = temp;
+	}
+}
