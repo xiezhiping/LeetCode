39078dacd1562d65d4c3b9678c90d69cf77d3c99
xiezhiping <1529464401@qq.com>
2018-12-21 09:57:23 +0800

    note

--- src/sort/SelectSort.java
+++ src/sort/SelectSort.java
@@ -1,5 +1,6 @@
 package sort;
 /**
+ * 一种简单直观的排序算法：每次从无序中选一个最小的放到有序的末尾
  * 交换操作介于0~(n-1)之间；
  * 比较操作为(n-1)*n/2;
  * 赋值操作介于0~(n-1)*3；
@@ -10,7 +11,7 @@ package sort;
 public class SelectSort {
 
 	public static void main(String[] args) {
-		int[] a = {2, 5, 8, 9, 7, 6, 11, 10, -9, 3};
+		int[] a = {2, 5, 8, 9, 7, 6, 11, 10, -9, 3}; // 可根据自己需要改
 		int length = a.length; // 放到for循环不恰当
 		System.out.println("before sort:");
 		for (int i= 0; i < length; i++) {
