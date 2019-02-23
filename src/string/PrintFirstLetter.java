ff483323e6759b93a9e18a509e8fb3fa7aad3950
xiezhiping <1529464401@qq.com>
2019-02-23 15:18:02 +0800

    methodThree

--- src/string/PrintFirstLetter.java
+++ src/string/PrintFirstLetter.java
@@ -1,13 +1,14 @@
 package string;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
 
 public class PrintFirstLetter {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String target = scanner.next();
-		char result = findLetter2(target);
+		char result = findLetter3(target);
 		System.out.println(result);
 	}
 	/**
@@ -38,6 +39,11 @@ public class PrintFirstLetter {
 		}
 		return '0';
 	}
+	/**
+	 * 第二种用哈希表的方式来统计，时间复杂度为O(N)
+	 * @param string
+	 * @return
+	 */
 	public static char findLetter2(String string) {
 		int len = string.length();
 		HashMap<Character, Integer> hashMap = new HashMap<>();
@@ -54,6 +60,28 @@ public class PrintFirstLetter {
 		for (int i = 0; i < len; i++) {
 			if (hashMap.get(string.charAt(i)) == 1) {
 				return string.charAt(i);
+			}
+		}
+		return '0';
+	}
+	// 第三种利用LinkedHashMap自身有序的特性，使用迭代进行结果求取
+	public static char findLetter3(String string) {
+		int len = string.length();
+		LinkedHashMap<Character, Integer> linkedHashMap = new LinkedHashMap<>();
+		// 第一次遍历统计
+		for (int i = 0; i < len; i++) {
+			char temp = string.charAt(i);
+			if (linkedHashMap.containsKey(temp)) {
+				int value = linkedHashMap.get(temp);
+				linkedHashMap.put(temp, value + 1);
+			} else {
+				linkedHashMap.put(temp, 1);
+			}
+		}
+		// 再进行一次迭代器遍历
+		for(Character key : linkedHashMap.keySet()) {
+			if (linkedHashMap.get(key) == 1) {
+				return key;
 			}
 		}
 		return '0';
