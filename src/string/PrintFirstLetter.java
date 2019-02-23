1157b07e6d620b73a63cbff3b28b14322083bc76
xiezhiping <1529464401@qq.com>
2019-02-23 15:08:01 +0800

    methodTwo

--- src/string/PrintFirstLetter.java
+++ src/string/PrintFirstLetter.java
@@ -1,14 +1,20 @@
 package string;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class PrintFirstLetter {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String target = scanner.next();
-		char result = findLetter(target);
+		char result = findLetter2(target);
 		System.out.println(result);
 	}
+	/**
+	 * 通过类似hash表的方式来统计字符串出现次数，时间复杂度O(N)
+	 * @param string
+	 * @return
+	 */
 	public static char findLetter(String string) {
 		int len = string.length();
 		int[] count = new int[52]; // 用于记录对应字符出现的次数
@@ -28,6 +34,26 @@ public class PrintFirstLetter {
 				return temp;
 			} else if(count[temp - 71] == 1) {
 				return temp;
+			}
+		}
+		return '0';
+	}
+	public static char findLetter2(String string) {
+		int len = string.length();
+		HashMap<Character, Integer> hashMap = new HashMap<>();
+		// 第一次遍历是统计次数
+		for (int i = 0; i < len; i++) {
+			if (hashMap.containsKey(string.charAt(i))) {
+				int value = hashMap.get(string.charAt(i));
+				hashMap.put(string.charAt(i), value+1);
+			} else {
+				hashMap.put(string.charAt(i), 1);
+			}
+		}
+		// 第二次遍历是获取结果
+		for (int i = 0; i < len; i++) {
+			if (hashMap.get(string.charAt(i)) == 1) {
+				return string.charAt(i);
 			}
 		}
 		return '0';
